94eaa6849705c88cf58157d975ff306f7624f6f9
Juan Carlos Herrera Blandon <jhcarlos8888@gmail.com>
2023-03-19 13:37:24 -0500

    Se realiza adicion de directiva httpclient en modulo principal app.module.ts, se realiza utilizacion de esta directiva en la pagina customers, la cual consume un archivo json localmente al inicar el renderizado de la pagina customers

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # appIonicMenu
 App construida en Ionic que permitirá un manejo básico de operaciones para clientes
+
+# Recomendaciones para utiliza capacitor
+
+# Instalar capacitor en el proyecto
+
+
+npm i @capacitor/core
+npm i -D @capacitor/cli
+
+
+# Se inicializa archivo de configuracion de capacitor
+npx cap init
+
+
+# Se adiciona soporte de apps Android y Ios para la app
+
+npm i @capacitor/android @capacitor/ios
+
+# Se genera archivos compilados por medio de Ionic para utilizarse como app nativa
+
+ionic build
+
+# Se sincroniza archivos compilados con la arquitectura de android o Ios por medio de capacitor
+
+npx cap sync
--- src/app/customers/customers.page.ts
+++ src/app/customers/customers.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-customers',
@@ -8,13 +10,31 @@ import { Router } from '@angular/router';
 })
 export class CustomersPage implements OnInit {
 
-  constructor(private router: Router) { }
+  users: any = [];
+
+
+  constructor(private router: Router,
+    private http: HttpClient) { }
 
   ngOnInit() {
+    this.getUsers().subscribe((res) =>{
+      console.log("Respuesta de peticion",res)
+      this.users = res;
+    })
   }
 
   goToHome(){
     this.router.navigate(['/home'])
+  }
+
+  getUsers(){
+    return this.http
+    .get('assets/files/customers.json')
+    .pipe(
+      map((res:any) =>{
+        return res.data;
+      })
+    )
   }
 
 }
